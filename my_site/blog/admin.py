b2192477fc0eb88d69c4be47384c53311ad1ca72
Tácio de Souza Campos <taciodesouzacampos@hotmail.com>
2021-05-25 17:16:56 -0300

    adding database and fetching data from it

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'slug': ('title',)}
+  list_filter = ('author', 'tags',)
+  list_display = ('title', 'author', 'date',)
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Author(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField()
+
+  def full_name(self):
+    return f"{self.first_name} {self.last_name}"
+
+  def __str__(self):
+      return self.full_name()
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  date = models.DateField()
+  image = models.CharField(max_length=100)
+  excerpt = models.CharField(max_length=255)
+  content = models.TextField(validators=[MinLengthValidator(10)])
+  author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, related_name='posts')
+  slug = models.SlugField()
+  tags = models.ManyToManyField('Tag', related_name='posts')
+
+  def __str__(self):
+    return f"{self.title} - {self.author}"
+
+
+class Tag(models.Model):
+  caption = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.caption
+  
